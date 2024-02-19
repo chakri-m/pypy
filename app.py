58637b9a9bf3347c5534e6e2fd7da46707c642d0
Mo-Molengeek <mohammed@molengeek.com>
2024-02-19 13:59:59 +0100

    commit1

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "1Helllok Worlerd this is mmgMora th jmbest AWS Engineer!!!!!"
+    return "1Helllok Worlerd this is mmgkMora th jmbest AWS Engineer!!!!!"
 if __name__ == "__main__":
     app.run()
