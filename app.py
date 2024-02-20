cfe14b8557a49e1d9c32ab67e56964727a1bc323
Mo-Molengeek <mohammed@molengeek.com>
2024-02-20 20:28:10 +0100

    add github Actions pipeline

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello peoople tfhis is MMorad the best AWS Engineer!!!!!"
+    return "Hello peooople tfhis is MMorad the best AWS Engineer!!!!!"
 if __name__ == "__main__":
     app.run()
