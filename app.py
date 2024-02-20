c1a459d3a679d7ea15cccfb493df768d5104b8cc
Mo-Molengeek <mohammed@molengeek.com>
2024-02-20 20:09:36 +0100

    delete cd pypy

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello coucou Worlgerd tfhis is MMorad the best AWS Engineer!!!!!"
+    return "Hello people Worlgerd tfhis is MMorad the best AWS Engineer!!!!!"
 if __name__ == "__main__":
     app.run()
