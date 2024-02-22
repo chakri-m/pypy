d9e7f7589191f7a5cf84a3cdfe1333be7641adc0
Mo-Molengeek <mohammed@molengeek.com>
2024-02-22 15:47:08 +0100

    add Morad

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello peooople tfhis is MMorad the best AWS Engineer!!!!!"
+    return "Hello peooople this is MMorad the Greatest AWS Engineer!!!!!"
 if __name__ == "__main__":
     app.run()
